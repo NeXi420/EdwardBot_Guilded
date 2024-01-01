da2c4dca33c5b63835937956e784aebdca6aa7f9
Patryk360 <nexigamingyt@gmail.com>
2024-01-01 16:29:44 +0100

    0.0.5

--- classes/Client.js
+++ classes/Client.js
@@ -1,6 +1,5 @@
 "use strict";
 const { Client, CacheCollection } = require("guilded.js");
-const connect = require("../database/connect.js");
 const config = require("../config/config.js");
 class Bot extends Client {
     constructor() {
